db01fb85d3ac5bc0d3bf67b4ae5231ce042d350c
zahra.hadadi <zahra.hadadi@student.ksgyf.se>
2026-05-06 11:04:44 +0200

    forloop

---
+++ forloop.ps1
@@ -0,0 +1,11 @@
+# loop för att räkna från 1 till 5
+  
+$start = Get-Date
+for ($i = 1; $i -le 5000; $i++) {
+    Write-Host "nummer är: $i"
+}
+ 
+$end = Get-Date
+
+$diff = $end - $start
+$diff.TotalSeconds
